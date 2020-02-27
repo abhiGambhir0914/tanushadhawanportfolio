ef860fa3b5d69df2787e7280e8668147eec1fdfd
Abhi Gambhir <abhigambhir97@gmail.com>
2020-02-27 11:02:44 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 9090;
+
+app.use(express.static('tanusha'));
+
+//body-parser middleware
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.sendFile('about.html',{root: __dirname + '/tanusha' });
+});
+
+app.listen(port, () => console.log(`Server set up at ${port}`));
